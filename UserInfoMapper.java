ce3d6fd3b8ea053ce594830717198a626b84ccf0
市川愛望 <167387698+ichikawa-manami@users.noreply.github.com>
2024-05-02 09:53:24 +0900

    ユーザーの検索情報をデータベースからアクセスする機能

---
+++ UserInfoMapper.java
@@ -0,0 +1,40 @@
+package com.example.mybatis.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.mybatis.dto.UserAddRequest;
+import com.example.mybatis.dto.UserSearchRequest;
+import com.example.mybatis.dto.UserUpdateRequest;
+import com.example.mybatis.entity.UserInfo;
+
+/**
+ * ユーザー情報 Mapper
+ */
+@Mapper
+public interface UserInfoMapper {
+
+    /**
+     * ユーザー情報全件検索
+     * @param user 検索用リクエストデータ
+     * @return 検索結果
+     */
+    List<UserInfo> findAll();
+  
+   /**
+     * ユーザー情報主キー検索
+     * @param id 主キー
+     * @return 検索結果
+     */
+    UserInfo findById(Long id);
+
+    /**
+     * ユーザー情報検索
+     * @param user 検索用リクエストデータ
+     * @return 検索結果
+     */
+    List<UserInfo> search(UserSearchRequest user);
+
+   
+}
